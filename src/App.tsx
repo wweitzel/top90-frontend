06a7cc94653b231ec2e90b922537707ade57abc5
Wes Weitzel <wesweitzel@gmail.com>
2023-01-20 23:46:49 -0800

    Make copy button copy the message preview link (#27)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Goal from './pages/Goal';
 import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
 
 export const BASE_URL = process.env.REACT_APP_TOP90_BASE_URL || 'http://localhost:3000';
+export const API_BASE_URL = process.env.REACT_APP_TOP90_API_BASE_URL || 'http://127.0.0.1:7171';
 
 const router = createBrowserRouter([
   {
