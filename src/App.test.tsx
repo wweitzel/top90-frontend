22843d2755adf455bf3b7c5d2625cfc2fc79d49e
Wes Weitzel <wesweitzel@gmail.com>
2023-03-11 15:53:06 -0800

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,5 +2,22 @@ import {render} from '@testing-library/react';
 import App from './App';
 
 test('renders the app with no errors', () => {
+  // window.matchMedia is not implemented in JSDOM so need to mock it
+  // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+
   render(<App />);
 });
