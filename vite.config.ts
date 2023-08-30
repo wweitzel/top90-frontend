cb66e6222d487a484eed2b4df73957ef3c214b4e
GIT-Gizmo <boluforjesus@gmail.com>
2023-08-30 23:16:01 +0100

    Successfully migrated from CRA(create-react-app) to Vite

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
+import reactRefresh from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  plugins: [
+    reactRefresh(),
+    viteTsconfigPaths(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+});
