2ce5035c649391c8758d1a55f855b4bec896a415
GIT-Gizmo <boluforjesus@gmail.com>
2023-08-31 21:12:44 +0100

    Reformatted the source code and removed vite-env.d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import reactRefresh from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
   plugins: [
     reactRefresh(),
