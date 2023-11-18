fe2c20811024d9b5eb8020170e9579cd962a56f2
astone123 <a.stone2695@gmail.com>
2023-11-18 11:23:04 -0800

    format

--- src/lib/api/core.ts
+++ src/lib/api/core.ts
@@ -1,5 +1,6 @@
 export const API_BASE_URL = import.meta.env.VITE_TOP90_API_BASE_URL || 'https://api.top90.io';
-export const PUBLIC_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : "https://top90.io"
+export const PUBLIC_URL =
+  import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://top90.io';
 export interface Pagination {
   skip: number;
   limit: number;
