103a60a011e248d532f6055ab3cde3c27f258689
Adam Stone-Lord <a.stone2695@gmail.com>
2023-12-16 18:21:40 -0700

    fix: add blocking script to load theme (#93)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ export function getPreferredTheme() {
     return storedTheme;
   }
 
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark';
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 }
 
 export function setTheme(theme: string) {
