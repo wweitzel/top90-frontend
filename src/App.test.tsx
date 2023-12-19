56d75fadee17c48b2989bc57651b18a0b1f7579d
astone123 <a.stone2695@gmail.com>
2023-12-18 21:14:37 -0700

    Revert "fix tests"
    This reverts commit 75426219465200b5497269e148b0a8c2461cdb7d.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,8 @@
 import './mocks/matchMedia.ts';
 
 import {render} from '@testing-library/react';
-import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
 test('renders the app with no errors', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  render(<App />);
 });
